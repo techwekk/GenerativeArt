15cd7e424521e67aca0e299e35cdc04082a7a11f
techwekk <t.wekker@hotmail.com>
2021-10-17 21:13:14 +0200

    pics

--- content/day01/01/sketch.js
+++ content/day01/01/sketch.js
@@ -16,7 +16,6 @@ function draw() {
 
   
 // you can change the stepSize
-//var stepSize = stepSize_slider.value();
 var stepSize = floor(map(mouseX, 0, width, 3, 3));
 for (var x = 0; x < video.width; x += stepSize) {
   for (var y = 0; y < video.height; y += stepSize) {
@@ -25,13 +24,11 @@ for (var x = 0; x < video.width; x += stepSize) {
     var redVal = video.pixels[index];
     var greenVal = video.pixels[index + 1];
     var blueVal = video.pixels[index + 2];
-    // you can add or remove the stroke
-    //strokeWeight(1);
-    //stroke(255,0,255,255);
+    // remove the stroke
     noStroke();
-    // you can change the colors
-    fill(redVal, greenVal, blueVal/2);
-    // you can change the shape of the 'pixels'
+    // change the colors
+    fill(redVal, greenVal, blueVal);
+    // change the shape of the 'pixels'
     rectMode(CENTER);
     rect(x, y, stepSize, stepSize);
     //circle(x, y, stepSize, stepSize);
